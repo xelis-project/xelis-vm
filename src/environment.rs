af32233d6be87e823ef7dad0f62127d3bd1a5a0a
Slixe <slixeprivate@gmail.com>
2022-03-05 18:13:14 +0100

    implement string functions

--- src/environment.rs
+++ src/environment.rs
@@ -25,15 +25,20 @@ impl Environment {
 
         // Array
         env.register_native_function(NativeFunction::new("len".to_owned(), Some(Type::Array(Box::new(Type::Any))), vec![], len, 1, Some(Type::Int)));
-        env.register_native_function(NativeFunction::new("push".to_owned(), Some(Type::Array(Box::new(Type::Any))), vec![Type::Any], push, 1, None));
+        env.register_native_function(NativeFunction::new("push".to_owned(), Some(Type::Array(Box::new(Type::Any))), vec![Type::Any], push, 1, None)); // TODO Any
         env.register_native_function(NativeFunction::new("remove".to_owned(), Some(Type::Array(Box::new(Type::Any))), vec![], remove, 1, Some(Type::Any)));
         env.register_native_function(NativeFunction::new("slice".to_owned(), Some(Type::Array(Box::new(Type::Any))), vec![Type::Int, Type::Int], slice, 3, Some(Type::Array(Box::new(Type::Any)))));
 
-        // String TODO
+        // String
         env.register_native_function(NativeFunction::new("len".to_owned(), Some(Type::String), vec![], len, 1, Some(Type::Int)));
-        /*functions.push(NativeFunction::new("trim".to_owned(), Some(Type::String), vec![], println, Some(Type::String)));
-        functions.push(NativeFunction::new("contains".to_owned(), Some(Type::String), vec![Type::String], println, Some(Type::Boolean)));
-        functions.push(NativeFunction::new("contains_ignore_case".to_owned(), Some(Type::String), vec![Type::String], println, Some(Type::Boolean)));
+        env.register_native_function(NativeFunction::new("trim".to_owned(), Some(Type::String), vec![], trim, 1, Some(Type::String)));
+        env.register_native_function(NativeFunction::new("contains".to_owned(), Some(Type::String), vec![Type::String], contains, 1, Some(Type::Boolean)));
+        env.register_native_function(NativeFunction::new("contains_ignore_case".to_owned(), Some(Type::String), vec![Type::String], contains_ignore_case, 1, Some(Type::Boolean)));
+        env.register_native_function(NativeFunction::new("to_uppercase".to_owned(), Some(Type::String), vec![], to_uppercase, 1, Some(Type::String)));
+        env.register_native_function(NativeFunction::new("to_lowercase".to_owned(), Some(Type::String), vec![], to_lowercase, 1, Some(Type::String)));
+        env.register_native_function(NativeFunction::new("to_bytes".to_owned(), Some(Type::String), vec![], to_bytes, 5, Some(Type::Array(Box::new(Type::Byte)))));
+
+        /*
         functions.push(NativeFunction::new("index_of".to_owned(), Some(Type::String), vec![Type::String], println, Some(Type::Int)));
         functions.push(NativeFunction::new("last_index_of".to_owned(), Some(Type::String), vec![Type::String], println, Some(Type::Int)));
         functions.push(NativeFunction::new("replace".to_owned(), Some(Type::String), vec![Type::String, Type::String], println, Some(Type::String)));
@@ -41,8 +46,7 @@ impl Environment {
         functions.push(NativeFunction::new("ends_with".to_owned(), Some(Type::String), vec![Type::String], println, Some(Type::Boolean)));
         functions.push(NativeFunction::new("split".to_owned(), Some(Type::String), vec![Type::String], println, Some(Type::Array(Box::new(Type::String)))));
         functions.push(NativeFunction::new("char_at".to_owned(), Some(Type::String), vec![Type::Int], println, Some(Type::String)));
-        functions.push(NativeFunction::new("to_upper".to_owned(), Some(Type::String), vec![], println, Some(Type::String)));
-        functions.push(NativeFunction::new("to_lower".to_owned(), Some(Type::String), vec![], println, Some(Type::String)));
+
         functions.push(NativeFunction::new("is_empty".to_owned(), Some(Type::String), vec![], println, Some(Type::Boolean)));
         functions.push(NativeFunction::new("matches".to_owned(), Some(Type::String), vec![Type::String], println, Some(Type::Boolean)));
         functions.push(NativeFunction::new("substring".to_owned(), Some(Type::String), vec![Type::Int], println, Some(Type::String)));
@@ -122,4 +126,43 @@ fn println(_: FnInstance, mut parameters: Vec<Value>) -> FnReturnType {
     println!("{}", param);
 
     Ok(None)
+}
+
+// string
+fn trim(zelf: FnInstance, _: Vec<Value>) -> FnReturnType {
+    let s = zelf?.as_string()?.trim().to_string();
+    Ok(Some(Value::String(s)))
+}
+
+fn contains(zelf: FnInstance, mut parameters: Vec<Value>) -> FnReturnType {
+    let value = parameters.remove(0).to_string()?;
+    let s: &String = zelf?.as_string()?;
+    Ok(Some(Value::Boolean(s.contains(&value))))
+}
+
+fn contains_ignore_case(zelf: FnInstance, mut parameters: Vec<Value>) -> FnReturnType {
+    let value = parameters.remove(0).to_string()?.to_lowercase();
+    let s: String = zelf?.as_string()?.to_lowercase();
+    Ok(Some(Value::Boolean(s.contains(&value))))
+}
+
+fn to_uppercase(zelf: FnInstance, _: Vec<Value>) -> FnReturnType {
+    let s: String = zelf?.as_string()?.to_uppercase();
+    Ok(Some(Value::String(s)))
+}
+
+fn to_lowercase(zelf: FnInstance, _: Vec<Value>) -> FnReturnType {
+    let s: String = zelf?.as_string()?.to_lowercase();
+    Ok(Some(Value::String(s)))
+}
+
+fn to_bytes(zelf: FnInstance, _: Vec<Value>) -> FnReturnType {
+    let s: &String = zelf?.as_string()?;
+
+    let mut bytes: Vec<RefValue> = Vec::new();
+    for byte in s.as_bytes() {
+        bytes.push(Rc::new(RefCell::new(Value::Byte(*byte))));
+    }
+
+    Ok(Some(Value::Array(bytes)))
 }
