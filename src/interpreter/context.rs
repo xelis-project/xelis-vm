0c83a678be4ba1206e58d71db05f8b9388efcee3
Slixe <slixeprivate@gmail.com>
2024-08-16 01:12:52 +0200

    interpreter: test NoOpHasher as HashMap hasher

--- src/interpreter/context.rs
+++ src/interpreter/context.rs
@@ -1,8 +1,27 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::{BuildHasherDefault, Hasher}};
 
 use crate::{InterpreterError, IdentifierType, values::*};
 
-pub type Scope = HashMap<IdentifierType, ValueVariant>;
+pub type Scope = HashMap<IdentifierType, ValueVariant, BuildHasherDefault<NoOpHasher>>;
+
+// Hasher that does nothing
+// Because we have u16 as the key, we don't need to hash it
+#[derive(Debug, Default)]
+pub struct NoOpHasher(u16);
+
+impl Hasher for NoOpHasher {
+    fn write(&mut self, bytes: &[u8]) {
+        self.0 = u16::from_le_bytes([bytes[0], bytes[1]]);
+    }
+
+    fn write_u64(&mut self, _: u64) {
+        unimplemented!("write_u64")
+    }
+
+    fn finish(&self) -> u64 {
+        self.0 as u64
+    }
+}
 
 #[derive(Debug)]
 pub struct Context {
@@ -53,7 +72,7 @@ impl Context {
 
     // Create a new scope
     pub fn begin_scope(&mut self) {
-        self.scopes.push(HashMap::new());
+        self.scopes.push(Scope::default());
     }
 
     // End the latest scope created
