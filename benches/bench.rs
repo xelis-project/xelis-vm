a10e1b4ba144829bd2297e1100955d54589a6e66
Slixe <slixeprivate@gmail.com>
2024-09-27 16:29:28 +0200

    bench: rework

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,61 +4,97 @@ use xelis_vm::{
     bytecode::{compiler::Compiler, vm::VM, Module},
     Environment,
     EnvironmentBuilder,
+    IdentifierType,
     Interpreter,
     Lexer,
     Parser,
+    Program,
     State
 };
 
-const CODE: &str = "
+const DEFAULT_CODE: &str = "
 entry main() {
     let a: u8 = 10;
     let b: u8 = 20;
     let c: u8 = a + b;
     return c as u64;
-}
-";
+}";
 
-fn bench_lexer(c: &mut Criterion) {
-    c.bench_function("lexer", |b| b.iter(|| Lexer::new(CODE).get().unwrap()));
-}
+// Find 2000 prime numbers
+const PRIME_FINDER: &str = "
+entry main() {
+    let primes: u64[] = [2];
+    let check: u64 = 3;
+    let index: u64 = 1;
+    while index < 2_000 {
+        let is_prime: bool = true;
+        foreach prime in primes {
+            if (check % prime) == 0 {
+                is_prime = false;
+                break;
+            }
+        }
 
-fn bench_parser(c: &mut Criterion) {
-    let tokens = Lexer::new(CODE).get().unwrap();
-    let env = EnvironmentBuilder::new();
-    c.bench_function("parser", |b| b.iter(|| Parser::new(tokens.clone(), &env).parse().unwrap()));
-}
+        if is_prime {
+            primes.push(check);
+            index += 1;
+        }
 
-fn bench_interpreter(c: &mut Criterion) {
-    let tokens = Lexer::new(CODE).get().unwrap();
-    let env = EnvironmentBuilder::new();
-    let (program, mapper) = Parser::new(tokens, &env).parse().unwrap();
-    let interpreter = Interpreter::new(&program, env.environment()).unwrap();
-    let mut state = State::new(None, Some(100), None);
-    let signature = Signature::new("main".to_owned(), None, Vec::new());
-    c.bench_function("interpreter", |b| b.iter(|| interpreter.call_entry_function(&mapper.get(&signature).unwrap(), vec![], &mut state).unwrap()));
-}
+        check += 2
+    }
 
-fn prepare_module(code: &str) -> (Module, Environment) {
+    return 0;
+}";
+
+fn prepare(code: &str) -> (IdentifierType, Program, Module, Environment) {
     let tokens = Lexer::new(code).get().unwrap();
-    let env = EnvironmentBuilder::new();
-    let (program, _) = Parser::new(tokens, &env).parse().unwrap();
+    let env = EnvironmentBuilder::default();
+    let (program, mapper) = Parser::new(tokens, &env).parse().unwrap();
+    
+    let signature = Signature::new("main".to_owned(), None, Vec::new());
+    let entry = mapper.get(&signature).unwrap();
 
     let env = env.build();
     let module = Compiler::new(&program, &env).compile().unwrap();
 
-    (module, env)
+    (entry, program, module, env)
 }
 
-fn bench_vm(c: &mut Criterion) {
-    let (module, environment) = prepare_module(CODE);
-    let mut vm = VM::new(&module, &environment);
+fn bench_internal(c: &mut Criterion, code: &str, name: &str) {
+    let mut group = c.benchmark_group(name);
 
-    c.bench_function("vm", |b| b.iter(|| {
+    // Lexer
+    group.bench_function("lexer", |b| b.iter(|| Lexer::new(code).get().unwrap()));
+
+    // Parser
+    let tokens = Lexer::new(code).get().unwrap();
+    let env = EnvironmentBuilder::default();
+    group.bench_function("parser", |b| b.iter(|| Parser::new(tokens.clone(), &env).parse().unwrap()));
+
+    let (entry, program, module, environment) = prepare(code);
+
+    // Interpreter
+    let mut state = State::new(None, Some(100), None);
+    let interpreter = Interpreter::new(&program, &environment).unwrap();
+    group.bench_function("interpreter", |b| b.iter(|| {
+        interpreter.call_entry_function(&entry, vec![], &mut state).unwrap();
+    }));
+
+    // VM
+    let mut vm = VM::new(&module, &environment);
+    group.bench_function("vm", |b| b.iter(|| {
         vm.invoke_chunk_id(0).unwrap();
         vm.run().unwrap();
     }));
 }
 
-criterion_group!(benches, bench_lexer, bench_parser, bench_interpreter, bench_vm);
+fn bench_simple_code(c: &mut Criterion) {
+    bench_internal(c, DEFAULT_CODE, "simple_code");
+}
+
+fn bench_prime_finder(c: &mut Criterion) {
+    bench_internal(c, PRIME_FINDER, "prime_finder");
+}
+
+criterion_group!(benches, bench_simple_code, bench_prime_finder);
 criterion_main!(benches);
