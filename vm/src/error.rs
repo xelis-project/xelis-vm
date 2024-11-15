151744564ea113726c8ba3ae176b267787d8aeaf
Slixe <slixeprivate@gmail.com>
2024-11-15 17:29:51 +0100

    vm: thiserror dependency

--- vm/src/error.rs
+++ vm/src/error.rs
@@ -1,35 +1,56 @@
+use thiserror::Error;
 use xelis_environment::EnvironmentError;
 use xelis_types::{Value, ValueError};
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum VMError {
+    #[error("enum not found")]
     EnumNotFound,
+    #[error("enum variant not found")]
     InvalidEnumVariant,
+    #[error("stack not cleaned")]
     StackNotCleaned,
+    #[error("invalid range type")]
     InvalidRangeType,
+    #[error("empty stack")]
     EmptyStack,
+    #[error("incompatible values: {0:?} and {1:?}")]
     IncompatibleValues(Value, Value),
+    #[error("chunk was not found")]
     ChunkNotFound,
+    #[error("chunk is not an entry")]
     ChunkNotEntry,
+    #[error("struct was not found")]
     StructNotFound,
+    #[error("missing instruction in module")]
     MissingInstruction,
+    #[error("invalid opcode")]
     InvalidOpCode,
-    TryInto,
+    #[error("invalid primitive type")]
     InvalidPrimitiveType,
+    #[error("register was not found")]
     RegisterNotFound,
+    #[error("empty register")]
     EmptyRegister,
-    MissingConstant,
-    NoValue,
+    #[error("constant not found")]
     ConstantNotFound,
+    #[error("unsupported cast type")]
     UnsupportedCastType,
+    #[error("unsupported syscall operation")]
     UnknownSysCall,
+    #[error("error from environment: {0}")]
     EnvironmentError(EnvironmentError),
+    #[error("error from value: {0}")]
     ValueError(ValueError),
-    IncrementError,
+    #[error("empty iterator")]
     EmptyIterator,
+    #[error("stack index out of bounds")]
     StackIndexOutOfBounds,
+    #[error("not enough arguments")]
     NotEnoughArguments,
+    #[error("stack overflow")]
     StackOverflow,
+    #[error("call stack overflow")]
     CallStackOverflow
 }
 
