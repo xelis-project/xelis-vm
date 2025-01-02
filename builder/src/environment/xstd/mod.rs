46c3a8035240592faf50d415cdb3bfa18e8869a9
Tritonn204 <tritonn204@gmail.com>
2025-01-02 15:17:29 -0800

    set xstd require on_call to require() instead of panic()

--- builder/src/environment/xstd/mod.rs
+++ builder/src/environment/xstd/mod.rs
@@ -26,7 +26,7 @@ pub fn register(env: &mut EnvironmentBuilder) {
     env.register_native_function(&[], "println", None, vec![("value", Type::Any)], println, 1, None);
     env.register_native_function(&[], "debug", None, vec![("value", Type::Any)], debug, 1, None);
     env.register_native_function(&[], "panic", None, vec![("value", Type::Any)], panic, 1, Some(Type::Any));
-    env.register_native_function(&[], "require", None, vec![("value", Type::Any), ("value", Type::Any)], panic, 1, Some(Type::Any));
+    env.register_native_function(&[], "require", None, vec![("value", Type::Any), ("value", Type::Any)], require, 1, Some(Type::Any));
     env.register_native_function(&[], "assert", None, vec![("value", Type::Bool)], assert, 1, None);
     env.register_native_function(&[], "is_same_ptr", None, vec![("value1", Type::Any), ("value2", Type::Any)], is_same_ptr, 5, Some(Type::Bool));
 }
